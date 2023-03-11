c96701d023d3329e48878df54009905dbf47c2e1
rezaul choudhury <rofy2009@gmail.com>
2023-03-11 23:57:04 +0000

    first commit

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    fontLoaders: [
+      {
+        loader: "@next/font/google",
+        options: {
+          subsets: [
+              "latin"
+          ]
+        }
+      }
+    ]
+  },
+    // images: {
+    //   domains: ['image.tmdb.org']
+    // },
+}
+
+module.exports = nextConfig
